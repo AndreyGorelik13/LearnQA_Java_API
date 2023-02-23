67f5e80d6899e05af6c783b97441db30d0d5fea3
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2023-02-23 14:54:08 +0300

    HelloFromAndrey

--- src/test/java/HelloFromAndrey.java
+++ src/test/java/HelloFromAndrey.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloFromAndrey {
@@ -5,5 +7,13 @@ public class HelloFromAndrey {
     @Test
     public void testHelloFromAndrey(){
         System.out.println("Hello from Andrey");
+    }
+
+    @Test
+    public void testGetTextApi(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
