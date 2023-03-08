9327156c4df7b28c39073bf089a2a8d298196178
Горелик  Андрей <Andrey.Gorelik@fors.ru>
2023-03-09 00:32:03 +0300

    Ex13: User Agent

--- src/test/java/HelloFromAndrey.java
+++ src/test/java/HelloFromAndrey.java
@@ -129,22 +129,22 @@ public class HelloFromAndrey {
                     .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
                     .andReturn();
 
-           String serverResponse = checkCookieAut.asString();
-           if (Objects.equals(serverResponse, "You are authorized")){
-               System.out.println(password);
-               System.out.println(serverResponse);
-           }
+            String serverResponse = checkCookieAut.asString();
+            if (Objects.equals(serverResponse, "You are authorized")) {
+                System.out.println(password);
+                System.out.println(serverResponse);
+            }
         }
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"моделированный", "труднорастворимые"})
-    public void testStringLengthCheck(String name){
+    public void testStringLengthCheck(String name) {
         assertTrue(name.length() > 15, "The number of characters is less than 15. Name length = " + name.length());
     }
 
     @Test
-    public void testAssertCookie(){
+    public void testAssertCookie() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_cookie")
                 .andReturn();
@@ -156,7 +156,7 @@ public class HelloFromAndrey {
     }
 
     @Test
-    public void testAssertHeader(){
+    public void testAssertHeader() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_header")
                 .andReturn();
@@ -165,5 +165,52 @@ public class HelloFromAndrey {
         String headerValue = response.header("x-secret-homework-header");
         assertTrue(allHeaders.hasHeaderWithName("x-secret-homework-header"), "Response doesn't have 'x-secret-homework-header' header");
         assertEquals("Some secret value", headerValue, "Unexpected header");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+    })
+    public void testUserAgentCheck(String userAgent) {
+        Response response = RestAssured
+                .given()
+                .header("user-agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+        response.prettyPrint();
+
+        Map<String, String> userAgentResponseParam = new HashMap<>();
+        userAgentResponseParam.put("platform", response.jsonPath().get("platform"));
+        userAgentResponseParam.put("browser", response.jsonPath().get("browser"));
+        userAgentResponseParam.put("device", response.jsonPath().get("device"));
+
+        if (Objects.equals(userAgent, "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30")){
+            assertEquals("Mobile", userAgentResponseParam.get("platform"), "Incorrect value 'platform' " + userAgent);
+            assertEquals("No", userAgentResponseParam.get("browser"), "Incorrect value 'browser' " + userAgent);
+            assertEquals("Android", userAgentResponseParam.get("device"), "Incorrect value 'device' " + userAgent);
+        }
+        if (Objects.equals(userAgent, "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1")){
+            assertEquals("Mobile", userAgentResponseParam.get("platform"), "Incorrect value 'platform' " + userAgent);
+            assertEquals("Chrome", userAgentResponseParam.get("browser"), "Incorrect value 'browser' " + userAgent);
+            assertEquals("iOS", userAgentResponseParam.get("device"), "Incorrect value 'device' " + userAgent);
+        }
+        if (Objects.equals(userAgent, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")){
+            assertEquals("Googlebot", userAgentResponseParam.get("platform"), "Incorrect value 'platform' " + userAgent);
+            assertEquals("Unknown", userAgentResponseParam.get("browser"), "Incorrect value 'browser' " + userAgent);
+            assertEquals("Unknown", userAgentResponseParam.get("device"), "Incorrect value 'device' " + userAgent);
+        }
+        if (Objects.equals(userAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0")){
+            assertEquals("Web", userAgentResponseParam.get("platform"), "Incorrect value 'platform' " + userAgent);
+            assertEquals("Chrome", userAgentResponseParam.get("browser"), "Incorrect value 'browser' " + userAgent);
+            assertEquals("No", userAgentResponseParam.get("device"), "Incorrect value 'device' " + userAgent);
+        }
+        if (Objects.equals(userAgent, "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")){
+            assertEquals("Mobile", userAgentResponseParam.get("platform"), "Incorrect value 'platform' " + userAgent);
+            assertEquals("No", userAgentResponseParam.get("browser"), "Incorrect value 'browser' " + userAgent);
+            assertEquals("iPhone", userAgentResponseParam.get("device"), "Incorrect value 'device' " + userAgent);
+        }
     }
 }
